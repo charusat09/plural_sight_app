aed642b596cc8156c7d312ec4cea3129d97e4a66
Mayurkumar Patel <mayurt20@gmail.com>
2017-09-01 12:27:13 +0530

    complete the react reduct unidirectional data flow

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/CourseActions';
 
 class CoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: { title: "" }
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+    // this.courseRow = this.courseRow.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+    // dispatch is attached here because we didn't pass
+    // mapDispatchToProps in connect
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h1>Add Course</h1>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            onChange={this.onTitleChange}
+            value={this.state.course.title}
+            />
+        </div>
+        <input
+          type="submit"
+          className="btn"
+          onClick={this.onClickSave}
+          value="Add"
+          />
       </div>
     );
   }
 }
 
-export default CoursePage;
+CoursePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapToStateToProps(state, ownProps) {
+  return {
+    courses: state.courses //this is from rootReducer
+  };
+}
+
+export default connect(mapToStateToProps)(CoursePage);
